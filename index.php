669eb056db4be389fd6ee117cc9fa7e00fca04d5
Adenovi Fauzia Hanifah <adenovihanifah@mail.ugm.ac.id>
2022-11-06 15:31:20 +0700

    second commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+ echo "Hello World!<br>";
+
+ $nama = "Adenovi";
+ $umur = 21;
+
+ echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun. <br>";
+
+ $namaadik = "Hana";
+ $umuradik = 12;
+
+ echo "Nama adik saya <strong>$namaadik</strong>, adik saya berusia $umuradik tahun.<br>";
+
+ $selisihumur = $umur - $umuradik;
+ 
+ echo "Selisih usia saya dan adik saya adalah $selisihumur tahun";
+
+?>
