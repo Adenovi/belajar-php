a210f7e212a39d7fd729fad5ab4fb2e91f2a68ca
Adenovi Fauzia Hanifah <adenovihanifah@mail.ugm.ac.id>
2022-12-15 00:47:51 +0700

    first commit

--- index.php
+++ index.php
@@ -1,20 +1,58 @@
 <?php
+// membuat koneksi database
+$con = mysql_connect("localhost","root","fakultas");
 
- echo "Hello World!<br>";
+//mengecek koneksi mysql
+if(mysql_connect_error())|{
+    echo "koneksi gagal". mysql_connect_error();
+}else{
+    echo "koneksi berhasil"
+}
 
- $nama = "Adenovi";
- $umur = 21;
+//membaca data dari tabel mysql
+$query = "SELECT * from mahasiswa";
 
- echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun. <br>";
+//menjalankan mysql query untuk menampilkan data
+$result = mysql_query($con, $query);
+$mahasiswa = [];
+if ($result){
+    //menampilkan data
+    while($row = mysql_fetch_assoc($result)){
+        $mahasiswa [] = $row;
+    }
+    mysql_free_result($result);
+}
 
- $namaadik = "Hana";
- $umuradik = 13;
-
-
- echo "Nama adik saya <strong>$namaadik</strong>, adik saya berusia $umuradik tahun.<br>";
-
- $selisihumur = $umur - $umuradik;
- 
- echo "Selisih usia saya dan adik saya adalah $selisihumur tahun";
+//menutup koneksi mysql
+mysql_close($con);
+foreach($mahasiswa as $value){
+    echo $value["nama"];
+}
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Data Mahasiswa</title>
+</head>
+<body>
+    <h1> Data Mahasiswa </h1>
+    <?php var_dump($mahasiswa); ?>
+    <table border="1" style="width:100%;">
+        <tr>
+            <th>NIM</th>
+            <th>Nama</th>
+        </tr>
+        <?php foreach($mahasiswa as $value): ?>
+            <tr>
+            <th><?php echo $value["nim"]; ?></th>
+            <th><?php echo $value["nama"]; ?></th>
+        </tr>
+        <?php endforeach; ?>
+    </table>
+</body>
+</html>
